375b47846cba2179a9721fed5f3c674a24cf2bdd
m_SW319 <ace5447@naver.com>
2025-11-14 18:55:06 +0900

    Feat :: Tag 기능

--- src/component/jsx/MainBody/LeftSideBar/TagSearh.jsx
+++ src/component/jsx/MainBody/LeftSideBar/TagSearh.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import "../../../css/MainBody/LeftSideBar/TagSearch.css";
 function TagSearch({ isClose }) {
-  const [TagList] = useState([
+  const [TagList, setTagList] = useState([
     "기본 태그1",
     "기본 태그2",
     "태그3",
@@ -10,6 +10,18 @@ function TagSearch({ isClose }) {
     "태그6",
     "태그7",
   ]);
+
+  const [inputValue, setInputValue] = useState("");
+  const TagPlusHandler = () => {
+    if (inputValue.trim() === "") return; // 빈값 방지
+
+    setTagList((prev) => [...prev, inputValue]);
+    setInputValue(""); // 입력창 초기화
+  };
+
+  const DeleteTagHandler = (index) => {
+    setTagList((prev) => prev.filter((_, i) => i !== index));
+  };
 
   const handleWheel = (e) => {
     const container = e.currentTarget;
@@ -20,26 +32,52 @@ function TagSearch({ isClose }) {
 
   return (
     <>
-      {!isClose && (
-        <div
-          style={{
-            marginTop: "10px",
-            display: "flex",
-            alignItems: "flex-start",
-            gap: "10px",
-            height: "33px",
-          }}
-        >
-          <div className="close_Tag"> ✖ </div>
-          <div className="Tag-wrapper" onWheel={handleWheel}>
-            {TagList.map((tag, idx) => (
-              <div key={idx} className="Tag-Box">
-                {tag}
-              </div>
-            ))}
+      <div>
+        {!isClose && (
+          <div
+            style={{
+              marginTop: "10px",
+              display: "flex",
+              alignItems: "flex-start",
+              gap: "10px",
+              height: "33px",
+            }}
+          >
+            <div className="close_Tag"> ✖ </div>
+            <div className="Tag-wrapper" onWheel={handleWheel}>
+              {TagList.map((tag, idx) => (
+                <div key={idx} className="Tag-Box">
+                  {tag}
+                  <div
+                    style={{ marginLeft: "auto" }}
+                    className="deleteTag"
+                    onClick={() => DeleteTagHandler(idx)}
+                  >
+                    ✖
+                  </div>
+                </div>
+              ))}
+            </div>
           </div>
-        </div>
-      )}
+        )}
+
+        {!isClose && (
+          <div className="TagPlus-wrapper">
+            <div className="TagPlus">
+              <input
+                type="text"
+                className="inputTag"
+                placeholder="태그 입력"
+                value={inputValue}
+                onChange={(e) => setInputValue(e.target.value)}
+              />
+              <div className="PlusBtn" onClick={TagPlusHandler}>
+                ╋
+              </div>
+            </div>
+          </div>
+        )}
+      </div>
     </>
   );
 }
